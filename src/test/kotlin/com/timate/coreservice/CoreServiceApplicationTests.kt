efe6c62ba73dbda240304438fd39ca29c3e79428
Nicholas Dietz <nicholas.dietz@hotmail.com>
2022-10-13 23:46:17 +0200

    initial commit

---
+++ src/test/kotlin/com/timate/coreservice/CoreServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.timate.coreservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
